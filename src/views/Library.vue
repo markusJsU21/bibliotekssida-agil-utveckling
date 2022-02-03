1d127562b4920e5eb3dedbd9b2c964be348086e4
Rashadul <mohammed.haque@iths.se>
2022-02-03 14:52:10 +0100

    Pagination

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -3,18 +3,57 @@
     <div class="books-list">
       <h2>LIBRARY</h2>
       <router-link
-        v-for="book in books"
+        v-for="book in currentPage"
         :key="book.id"
         :to="'/Library/' + book.id"
       >
         {{ book.Title }}
       </router-link>
+      <div class="btn">
+        <button @click="prevPage">Previous Page</button>
+        <button @click="nextPage">Next Page</button>
+      </div>
+      
+      
     </div>
   </main>
 </template>
 
 <script>
 export default {
+
+  data(){
+    return{
+      start:0,
+      end:4,
+      currentPage:''
+    }
+  },
+
+  methods: {
+    nextPage(){
+      this.currentPage = this.books.slice(this.start,this.end)
+      if(this.end < this.books.length){
+       this.start += 4;
+        this.end += 4;
+      }
+      console.log(this.start + '' + this.end);
+
+    },
+    
+    
+     prevPage(){
+      this.currentPage = this.books.slice(this.start,this.end)
+      if(this.start > 0){
+       this.start -= 4;
+        this.end -= 4;
+      }
+      console.log(this.start + '' + this.end);
+
+    }
+
+
+  },
   computed: {
     books() {
       return this.$store.state.ChildrensBooks;
