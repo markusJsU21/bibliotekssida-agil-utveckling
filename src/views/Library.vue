5ca61e6eba1da37e8be3af72690281372aa9652b
Rashadul <mohammed.haque@iths.se>
2022-02-03 15:49:33 +0100

    Page changeing..

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -10,75 +10,42 @@
         {{ book.Title }}
       </router-link>
       <div class="btn">
-        <button @click="prevPage">Previous Page</button>
-        <button @click="nextPage">Next Page</button>
+        <button @click="prevPage()">prev</button>
+        <button @click="nextPage()">next</button>
       </div>
-      
-      
     </div>
-    <button @click="prevPage()">prev</button>
-    <button @click="nextPage()">next</button>
   </main>
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-
-  data(){
-    return{
-      start:0,
-      end:4,
-      currentPage:''
-    }
-  },
-
-  methods: {
-    nextPage(){
-      this.currentPage = this.books.slice(this.start,this.end)
-      if(this.end < this.books.length){
-       this.start += 4;
-        this.end += 4;
-      }
-      console.log(this.start + '' + this.end);
-
-    },
-    
-    
-     prevPage(){
-      this.currentPage = this.books.slice(this.start,this.end)
-      if(this.start > 0){
-       this.start -= 4;
-        this.end -= 4;
-      }
-      console.log(this.start + '' + this.end);
-
-    }
-
-
-  },
-=======
   data() {
     return {
-      currentPage: "",
       start: 0,
       end: 4,
+      currentPage: "",
     };
   },
+
   methods: {
     nextPage() {
       this.currentPage = this.books.slice(this.start, this.end);
-        this.end += 4;
+      if (this.end < this.books.length) {
         this.start += 4;
+        this.end += 4;
+      }
+      console.log(this.start + "" + this.end);
     },
+
     prevPage() {
-      this.end -= 4;
-      this.start -= 4;
       this.currentPage = this.books.slice(this.start, this.end);
+      if (this.start > 0) {
+        this.start -= 4;
+        this.end -= 4;
+      }
+      console.log(this.start + "" + this.end);
     },
   },
-
->>>>>>> 026e4150c1cc4aaf112b8abacac61c8abffd7cc8
   computed: {
     //hämntar från store och sparar i books()
     books() {
